ca5b2a7618430a3ce91bdd76d8b147cae7c6e4ba
KarthikSherigar20 <karthik.s@pococare.com>
2024-05-07 12:16:37 +0530

    new

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,39 @@
 const { defineConfig } = require("cypress");
-const xlsx=require('node-xlsx').default;  
-const fs=require('fs');
-const path=require('path');
+const xlsx = require("node-xlsx").default;
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
-reporter: 'cypress-mochawesome-reporter',
-// pageLoadTimeout: 180000,
-watchForFileChanges:false,
-video:true,
+  reporter: "cypress-mochawesome-reporter",
+
+  // pageLoadTimeout: 180000,
+  watchForFileChanges: false,
+
+  video: true,
+
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
-      on("task",{ parseXlsx({ filePath}){
-        return new Promise((resolve, reject)=>{
-          try{
-            const jsonData=xlsx.parse(fs.readFileSync(filePath))
-            resolve(jsonData);
-          }catch(e){
-            reject(e)
-          }
-        })
-      }})
+      on("task", {
+        parseXlsx({ filePath }) {
+          return new Promise((resolve, reject) => {
+            try {
+              const jsonData = xlsx.parse(fs.readFileSync(filePath));
+              resolve(jsonData);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        },
+      });
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "nuxt",
+      bundler: "webpack",
     },
   },
 });
---
+++ cypress/e2e/DataFlow/Medical Profile/Blood Group_AB-.cy.js
@@ -0,0 +1,41 @@
+import url from '../../../fixtures/urls.json';
+import un from '../../../fixtures/UN&PASS';
+import Elements from '../../../Objects/Elements';
+
+describe('BG',()=>{
+    it('BG',()=>{
+        const selectedEnvironments=url.selectedEnvironment;
+        const selectUrl=url.environments[selectedEnvironments];
+        cy.visit(selectUrl);
+        const P1=new Elements();
+        cy.wait(1500);
+        cy.contains('Login').click();
+        cy.wait(1500);
+        cy.get('input[id="email"]').type(un.Un);
+        cy.wait(1500);
+        cy.get('button[type="submit"]').click();
+        cy.wait(3500);
+        P1.PTA();
+        cy.wait(1000);
+        P1.OTP();
+        cy.wait(1000);
+        cy.contains('Verify').click();
+        cy.wait(1500);
+        cy.get('body').then(($bodyText)=>{
+            const bodyText=$bodyText.text();
+            if(bodyText.includes('Edit Profile')){
+                cy.contains('Edit Profile').click();
+            }else{
+            cy.contains('Complete Profile').eq(0).click();
+            }
+        })
+        cy.contains('Medical Profile').click();
+        cy.wait(1000);
+        let BG='select[class="chakra-select css-161pkch"]';
+        cy.wait(1000);
+        cy.get(BG).eq(0).select('AB-');
+        cy.wait(1000);
+        cy.get(BG).eq(0).should('have.value','AB-');
+
+    })
+})
