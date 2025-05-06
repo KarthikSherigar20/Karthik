58a2100c4b9d35e50fc0c8c6594bc73d21e6968c
KarthikSherigar20 <karthik.s@pococare.com>
2025-05-06 18:07:41 +0530

    06-05-2025

--- cypress/e2e/e2e/Manage user data/ManageuserData.cy.js
+++ cypress/e2e/e2e/Manage user data/ManageuserData.cy.js
@@ -18,18 +18,32 @@ describe('Adding,updating,deleting',()=>{
     
     const download= Cypress.config('downloads');
 
-    
-    
-    cy.contains(' Download Template').should('be.visible').click();
-
-    // cy.readFile(`${download}/createUser Tempate V2`).should('exist');
-
     cy.task('listFilesInDownloads',download).then((fileNames)=>{
         cy.log('filename',fileNames);
 
         expect(fileNames).to.include('createUser Tempate V2.xlsx');
     })
 
+    const downloadsFolder = Cypress.config('downloads');
+    const fileName = 'createUser Tempate V2.xlsx';
+    const filePath = `${downloadsFolder}/${fileName}`;
+    
+    cy.get('input[type="file"]').eq(0).attachFile(filePath);
+
+    cy.wait(500);
+
+    cy.get('select[class="chakra-select css-161pkch"]').select('Test');
+
+    cy.contains('Add Users').scrollIntoView().click();
+
+    cy.task('listFilesInDownloads', download).then((fileNames) => {
+        expect(
+          fileNames.some(file => 
+            file.toLowerCase() === 'add_user_modified_excel.xlsx'.toLowerCase()
+          )
+        ).to.be.true;
+      });
+    
 
     })
 })
