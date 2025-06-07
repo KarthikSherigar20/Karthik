a007b2c17fc29f3240199c8d4920bf3054622a7d
KarthikSherigar20 <karthik.s@pococare.com>
2025-06-07 11:57:33 +0530

    07-06-2025

--- cypress/e2e/Negative/Location1.cy.js
+++ cypress/e2e/Negative/Location1.cy.js
@@ -16,11 +16,36 @@ describe('Location', () => {
 
         cy.wait(1500);
         cy.contains('New').click();
-        cy.wait(2000);
-        cy.get('input[class="radioButton_radio__Dmg-B"]').eq(3).scrollIntoView().click();
-        cy.wait(2000);
-        cy.get('input[class="radioButton_radio__Dmg-B"]').eq(8).scrollIntoView().click();
-        cy.wait(2000);
+        cy.wait(600);
+        cy.get('div[class="container selectPatient_container__02wpR"]').find('input[name="radio"]').first().click();
+        cy.wait(600);
+
+        cy.get('div.container.selectPoc_wrapper__MBvMf', { timeout: 5000 }).then($div => {
+          if ($div.find('input[name="radio"]').length > 0) {
+            cy.wrap($div)
+              .find('input[name="radio"]')
+              .click();
+          } else {
+            // cy.log('Radio input not found — skipping click');
+            cy.get('div[class="container ticketBasicDetails_cardWrapper__Zq72v"]').find('p[class="text phoneNumber_value__Ktlo5"]')
+              .eq(1).invoke('text').then((mob) => {
+                const mo = mob.trim();
+
+                cy.wait(500);
+
+                cy.contains('Add Poc').scrollIntoView().click();
+
+                cy.wait(500);
+
+                cy.get('input[placeholder="POC Name"]').type('POC');
+
+                cy.get('input[placeholder="POC Mobile"]').type(mo)
+
+              })
+          }
+        });
+
+        cy.wait(600);
         cy.contains('Save Assessment').scrollIntoView().click();
         cy.wait(2000);
         cy.contains('Add New Location').scrollIntoView().click();
